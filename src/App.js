c20faf3d2d25945dbfb6afc57bdf56608400ec3d
unknown <mohamedh.harrat@gmail.com>
2021-03-03 20:13:04 +0100

    ajout cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './componenets/NavBar'
+import Cards from './componenets/Card'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Cards/>
     </div>
   );
 }
---
+++ src/componenets/Card.js
@@ -0,0 +1,21 @@
+function Cards () {
+    return(
+<div class="card">
+
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap"/>
+
+  <div class="card-body">
+
+    <h4 class="card-title"><a>Card title</a></h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+    <a href="#" class="btn btn-primary">Button</a>
+
+  </div>
+
+</div>
+    )
+}
+
+export default Cards
+
